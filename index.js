65fed82b791240bcfb96be3e017b7d5af98f3d0c
betsy <betsycates@gmail.com>
2025-05-21 11:06:08 -0400

    new design

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const port = process.env.PORT || 3000;
 // Define a route handler for the default home page
 app.get('/', (req, res) => {
   res.send(`
-    <div style="background: linear-gradient(135deg, #FFDAB9, #FFA07A, #FF7F50); height: 100vh; display: flex; flex-direction: column; justify-content: center; align-items: center; color: white; font-size: 36px; text-align: center; font-family: 'Roboto', sans-serif;">
-      <div style="margin-bottom: 20px;">Hello, welcome to <span style="font-weight: bold;">Headintheclouds!</span> :)</div>
-      <p style="font-size: 16px; font-weight: normal;">This is a Docker CI/CD pipeline tutorial using GitHub Actions to build and push images to Amazon ECR.</p>
+    <div style="background: linear-gradient(135deg, #2a2d3e, #2a2d3e); height: 100vh; display: flex; flex-direction: column; justify-content: center; align-items: center; font-family: 'Segoe UI', sans-serif; color:rgb(255, 255, 255); text-align: center;">
+      <h1 style="font-size: 40px; margin-bottom: 30px;">Infrastructure as Code Demo</h1>
+      <div style="display: flex; gap: 20px; flex-wrap: wrap; justify-content: center;">
+        <div style="width: 80px; height: 80px; background-color: #256b6b; border-radius: 50%;"></div>
+        <div style="width: 80px; height: 80px; background-color: #ff914d;"></div>
+        <div style="width: 0; height: 0; border-left: 40px solid transparent; border-right: 40px solid transparent; border-bottom: 80px solid #713efd;"></div>
+        <div style="width: 80px; height: 80px; background-color: #ffde59; transform: rotate(45deg);"></div>
+      </div>
     </div>
   `);
-});
 
 // Start the server
 app.listen(port, () => {
